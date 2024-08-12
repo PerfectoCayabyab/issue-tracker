17c72f45856711cdd2e809479165a01a6824d3c1
Perfecto Cayabyab <perfectocayabyab@gmail.com>
2024-08-12 17:14:49 +0800

    Build the new issue page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
-import '@radix-ui/themes/styles.css';
-import { Theme } from '@radix-ui/themes';
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,8 +21,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Theme>
-        <NavBar />
-        <main>{children}</main>
+          <NavBar />
+          <main className="p-5">{children}</main>
         </Theme>
       </body>
     </html>
