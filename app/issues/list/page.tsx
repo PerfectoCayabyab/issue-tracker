e9b175cd186852afb2cd346691997a28b8504981
Perfecto Cayabyab <perfectocayabyab@gmail.com>
2024-08-18 18:46:01 +0800

    Last commit

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -46,11 +46,16 @@ const IssuesPage = async ({ searchParams }: Props) => {
   );
 };
 
-export const dynamic = "force-dynamic";
+
 
 export const metadata:Metadata ={
   title: 'Issue Tracker - Issue List',
   description: 'View all project issues'
 }
 
+
+
 export default IssuesPage;
+
+
+export const dynamic = "force-dynamic";
